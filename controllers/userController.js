7d86d1686b21a2f98d4caca07260267215c97d01
Raiyan109 <kraiyan109@gmail.com>
2023-11-19 12:34:06 +0600

    CRUD complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,32 @@
 const User = require('../models/userModel.js');
 
+// Get all
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({})
+        if (!users) {
+            return res.status(400).json({ error: 'No user found' })
+        }
+        return res.status(200).json(users)
+    } catch (error) {
+        return res.status(400).json({ error: 'No user' })
+    }
+}
+
+// GET single
+const getUser = async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await User.findById(id)
+        if (!user) {
+            return res.status(400).json({ error: 'No user found by this id' })
+        }
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(400).json({ error: 'No user found by this id' })
+    }
+}
+
 // Create
 const createUser = async (req, res) => {
     const { firstName, lastName, email, gender, avatar, domain, available } = req.body
@@ -13,4 +40,41 @@ const createUser = async (req, res) => {
     return res.status(200).json({ user })
 }
 
-module.exports = { createUser }
+// Update
+const updateUser = async (req, res) => {
+    const { id } = req.params
+    const { firstName, lastName, email, gender, avatar, domain, available } = req.body
+
+    try {
+        const user = await User.findByIdAndUpdate(id, {
+            firstName, lastName, email, gender, avatar, domain, available
+        })
+
+        if (!user) {
+            return res.status(400).json({ error: 'Cant update' })
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(400).json({ error: 'No user found by this id' })
+    }
+
+}
+
+// Delete
+const deleteUser = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const user = await User.findByIdAndDelete(id)
+        if (!user) {
+            return res.status(400).json({ error: 'Cant Delete by this id' })
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(400).json({ error: 'No user found by this id' })
+    }
+}
+
+module.exports = { createUser, getAllUsers, getUser, updateUser, deleteUser }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController');
+const { createUser, getAllUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
 
 const router = express.Router()
 
 router.post('/create', createUser)
+router.get('/', getAllUsers)
+router.get('/:id', getUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 module.exports = router
